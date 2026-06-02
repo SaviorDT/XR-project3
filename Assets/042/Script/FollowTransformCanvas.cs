6522a8b667a980daba769ea03989c0689fa7d84a
nycu139 <144760231+nycu139@users.noreply.github.com>
2026-06-02 20:18:54 +0800

    6/2
    scene0 + project setting
    解決字幕閃爍

--- Assets/042/Script/FollowTransformCanvas.cs
+++ Assets/042/Script/FollowTransformCanvas.cs
@@ -4,16 +4,12 @@ public class FollowTransformCanvas : MonoBehaviour
 {
     public Transform target;
 
-    public float distance = 2.0f;
+    public float distance = 1.0f;
     public float heightOffset = 0.0f;
     public Vector3 localOffset = Vector3.zero;
 
     public bool followYawOnly = true;
     public bool faceTarget = true;
-
-    public bool smoothFollow = true;
-    public float positionSmooth = 10f;
-    public float rotationSmooth = 10f;
 
     void LateUpdate()
     {
@@ -26,13 +22,11 @@ public class FollowTransformCanvas : MonoBehaviour
         if (forward.sqrMagnitude < 0.001f)
             forward = target.forward;
 
-        Vector3 targetPos =
+        transform.position =
             target.position +
             forward * distance +
             Vector3.up * heightOffset +
             target.TransformDirection(localOffset);
-
-        Quaternion targetRot;
 
         if (faceTarget)
         {
@@ -44,24 +38,13 @@ public class FollowTransformCanvas : MonoBehaviour
             if (dir.sqrMagnitude < 0.001f)
                 dir = forward;
 
-            targetRot = Quaternion.LookRotation(dir.normalized, Vector3.up);
+            transform.rotation = Quaternion.LookRotation(dir.normalized, Vector3.up);
         }
         else
         {
-            targetRot = followYawOnly
+            transform.rotation = followYawOnly
                 ? Quaternion.Euler(0f, target.eulerAngles.y, 0f)
                 : target.rotation;
-        }
-
-        if (smoothFollow)
-        {
-            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * positionSmooth);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotationSmooth);
-        }
-        else
-        {
-            transform.position = targetPos;
-            transform.rotation = targetRot;
         }
     }
 }
--- Assets/042/Script/ManualHUDRenderer.cs
+++ Assets/042/Script/ManualHUDRenderer.cs
@@ -3,22 +3,10 @@ using UnityEngine;
 public class ManualHUDRenderer : MonoBehaviour
 {
     public Camera hudRenderCamera;
-    public MonoBehaviour[] updateBeforeRender;
 
     void LateUpdate()
     {
-        for (int i = 0; i < updateBeforeRender.Length; i++)
-        {
-            if (updateBeforeRender[i] is IManualHUDUpdate updater)
-                updater.ManualHUDUpdate();
-        }
-
         if (hudRenderCamera != null)
             hudRenderCamera.Render();
     }
-}
-
-public interface IManualHUDUpdate
-{
-    void ManualHUDUpdate();
 }
